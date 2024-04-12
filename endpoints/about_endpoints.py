708adff72b7f5ebe4ae06f124ce9b94113cc17f8
Akash20010302 <akashpalm2017@gmail.com>
2024-04-12 13:50:00 +0530

    Commited

--- endpoints/about_endpoints.py
+++ endpoints/about_endpoints.py
@@ -19,49 +19,6 @@ about_router = APIRouter()
 connect(db='trace_about', host="mongodb://localhost:27017/")
 
 
-# class HouseholdIncomeDocument(EmbeddedDocument):
-#     candidate_monthly_take = IntField()
-#     spouse_monthly_take = IntField(default=0)
-#     total_family_income = IntField()
-
-# class InfoDocument(Document):
-#     application_id = IntField(required=True)
-#     page_id = IntField(required=True, default=1)
-#     firstName = StringField()
-#     middleName = StringField()
-#     lastName = StringField()
-#     phone = StringField()
-#     email = StringField()
-#     city = StringField(default="N/A")
-#     gender = StringField()
-#     dob = StringField()
-#     age = IntField()
-#     marital_status = StringField()
-#     spouse_work_status = StringField(default="N/A")
-#     spouse_employer = StringField(default="N/A")
-#     kidsnum = IntField(default=0)
-#     adultdependents = IntField()
-#     home = BooleanField()
-#     car = BooleanField()
-#     twoWheeler = BooleanField()
-#     creditCard = BooleanField()
-#     Loan = BooleanField()
-#     Investment = BooleanField()
-#     education = StringField()
-#     education_institute = StringField(default="N/A")
-#     location = StringField()
-#     total_experience = IntField()
-#     work_industry = StringField(default="N/A")
-#     skillset = StringField(default="N/A")
-#     current_role = StringField()
-#     tenure_last_job = BooleanField()
-#     household_income = EmbeddedDocumentField(HouseholdIncomeDocument)
-#     meta = {
-#         'indexes':[
-#             {'fields':['application_id', 'page_id'], 'unique': True}
-#         ]
-#     }
-
 
 mandatory_fields = ["firstName", "lastName", "phone", "email", "dob", "age", "gender", "marital_status", "education", "experience", "city", "salary"]
 @about_router.get("/about_user/{id}", tags=["About"])
